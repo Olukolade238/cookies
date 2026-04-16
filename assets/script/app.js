3a3571eab365b054af543fd317baabbc7bc0c1d0
Olukolade <olukoladekaka@gmail.com>
2026-04-16 17:29:41 -0500

    Working on JavaScript

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// QuerySelectors()
+const mainModal = document.querySelector('.modal');
+const infoBox = document.getElementById('cookieModal');
+const optionsBox = document.getElementById('settingsModal');
+const acceptAllBtn = document.querySelector('.primary');
+const openSettingsBtn = document.querySelector('.outline');
+const optBrowser = document.getElementById('browser');
+const optOS = document.getElementById('os');
+const optWidth = document.getElementById('width');
+const optHeight = document.getElementById('height');
+const saveSettingsBtn = document.getElementById("saveSettings");
+
+const cookieTime = 20;
