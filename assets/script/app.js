955bdc1dbb6b98884749ab68e9e979d3483cd2cc
Olukolade <olukoladekaka@gmail.com>
2026-04-16 17:33:05 -0500

    Working on JavaScript

--- assets/script/app.js
+++ assets/script/app.js
@@ -13,3 +13,22 @@ const optHeight = document.getElementById('height');
 const saveSettingsBtn = document.getElementById("saveSettings");
 
 const cookieTime = 20;
+
+// Set and get function 
+function setCookie(key, value) {
+    document.cookie = key + "=" + value + "; max-age=" + cookieTime + "; path=/";
+}
+
+function getCookie(key) {
+    const allCookies = document.cookie.split(";");
+
+    for (let part of allCookies) {
+        let clean = part.trim(); // remove space
+
+        if (clean.indexOf(key + "=") === 0) {
+            return clean.slice(key.length + 1);
+        }
+    }
+
+    return null;
+}
