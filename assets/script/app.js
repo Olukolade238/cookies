f8646a09efb62037aa13e1cc6398e6998589acf7
Olukolade <olukoladekaka@gmail.com>
2026-04-16 17:43:14 -0500

    Working on JavaScript

--- assets/script/app.js
+++ assets/script/app.js
@@ -61,3 +61,63 @@ function getOS() {
     }
     return "Other";
 }
+
+// When the page loads, this code checks if cookies are allowed and shows the cookie popup after a short delay if the user has not chosen yet.
+window.addEventListener("load", function () {
+    const existing = getCookie("userChoice");
+
+    if (!navigator.cookieEnabled) {
+        console.log("Cookies are off");
+        return;
+    }
+    if (!existing) {
+        setTimeout(function () {
+            mainModal.style.display = "flex";
+            infoBox.style.display = "flex";
+            optionsBox.style.display = "none";
+        }, 3000);
+    }
+});
+
+// This code runs when the “Accept all” button is clicked, saves the user’s browser, operating system, and screen size as cookies, records that the user accepted all cookies, and then closes the cookie popup.
+acceptAllBtn.onclick = function () {
+    setCookie("browser", getBrowser());
+    setCookie("os", getOS());
+    setCookie("width", screen.width);
+    setCookie("height", screen.height);
+
+    setCookie("userChoice", "all");
+
+    mainModal.style.display = "none";
+};
+
+// open setting
+openSettingsBtn.onclick = function () {
+    infoBox.style.display = "none";
+    optionsBox.style.display = "flex";
+};
+
+// Save setting
+saveSettingsBtn.onclick = function () {
+    let savedSomething = false;
+
+    if (optBrowser.checked) {
+        setCookie("browser", getBrowser());
+        savedSomething = true;
+    }   
+    if (optOS.checked) {
+        setCookie("os", getOS());
+        savedSomething = true;
+    }
+    if (optWidth.checked) {
+        setCookie("width", screen.width);
+        savedSomething = true;
+    }
+    if (optHeight.checked) {
+        setCookie("height", screen.height);
+        savedSomething = true;
+    }
+    setCookie("userChoice", savedSomething ? "custom" : "none");
+
+    mainModal.style.display = "none";
+};
